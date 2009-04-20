95ac1f94b5dd2ebf7f1fb704083113f729c142bc
Benoit Larroque <zeta.ben@gmail.com>
2009-04-20 15:16:56 +0200

    Parsing first try

--- lib/parser.rb
+++ lib/parser.rb
@@ -14,39 +14,61 @@ module HTML2FB
 			remove_objs(pdoc)
 			ti=pdoc.at('title')
 			doc.title= ti.inner_text.strip unless ti.nil?
-			pdoc.search('//h3').each do |e|
-				doc.content.push(e.inner_text)
-			end
+			#			pdoc.search('//h3').each do |e|
+			#				doc.content.push(e.inner_text)
+			#			end
+
+			doc.content=parse_text(pdoc.search('/html/body//'),pdoc,@conf)	
+
 			return doc
 		end
-	protected
+		protected
 
-	def remove_objs(doc)
-		if @conf['remove'] then
-			@conf['remove']['class'].each do |cl|
-				doc.search('.'+cl).remove
-			end unless @conf['remove']['class'].nil?
-			@conf['remove']['expr'].each do |cl|
-				doc.search(cl).remove
-			end unless @conf['remove']['expr'].nil?
-			@conf['remove']['before'].each do |cl|
-				x=doc.at(cl)
-				x.preceding.remove
-				x.parent.children.delete(x)
-			end unless @conf['remove']['before'].nil?
-			@conf['remove']['between'].each do |cl|
-				puts "between "+cl.inspect
-				doc.between(cl.first,cl.last).remove
-			end unless @conf['remove']['between'].nil?
-			@conf['remove']['after'].each do |cl|
-				x=doc.at(cl)
-				x.following.remove
-				x.parent.children.delete(x)
-			end unless @conf['remove']['after'].nil?
+		def remove_objs(doc)
+			if @conf['remove'] then
+				@conf['remove']['class'].each do |cl|
+					doc.search('.'+cl).remove
+				end unless @conf['remove']['class'].nil?
+				@conf['remove']['expr'].each do |cl|
+					doc.search(cl).remove
+				end unless @conf['remove']['expr'].nil?
+				@conf['remove']['before'].each do |cl|
+					x=doc.at(cl)
+					x.preceding.remove
+					x.parent.children.delete(x)
+				end unless @conf['remove']['before'].nil?
+				@conf['remove']['between'].each do |cl|
+					puts "between "+cl.inspect
+					doc.between(cl.first,cl.last).remove
+				end unless @conf['remove']['between'].nil?
+				@conf['remove']['after'].each do |cl|
+					x=doc.at(cl)
+					x.following.remove
+					x.parent.children.delete(x)
+				end unless @conf['remove']['after'].nil?
+			end
+			File.open('/tmp/test.html','w'){|f| f.write doc.to_html}
 		end
-		File.open('/tmp/test.html','w'){|f| f.write doc.to_html}
-	end
 
+		def parse_text(doc,root,conf=@conf)
+			return [doc,nil] if conf['select'].nil?
+			ret=[]
+			#puts conf['select'].inspect
+			points=root.search('//'+conf['select']['expr'])
+			#puts points.inspect
+			if points.size > 0 
+				p2=points.zip(points[1..-1]+[nil])
+				p2.each do |d,e|
+			#		puts d,e
+					el=root.between(d.xpath,(e ? e.xpath : nil) ) 
+					uu=doc-el
+			#		puts el
+					u,p = parse_text(el,root,conf['select'])
+					ret.push([uu+u,[p]])
+				end	
+			end
+			ret
+		end
 	end
 
 end
@@ -66,22 +88,23 @@ end
 
 module Hpricot::Traverse
 	def between(i,j)
-=begin		to=self.at(j).xpath
-		from=self.at(i).xpath
-		nodes=Hpricot::Elements[]
-		puts to
-		puts from
-		puts 
-		ok=false
-		self.at('/html').traverse_element do |el|
-			break if el.xpath == to
-			nodes << el if ok
-			puts el.xpath if ok
-			ok=true if el.xpath==from
+		#puts i,j
+		unless j.nil?
+			prec=self.at(i).preceding
+			Hpricot::Elements[*self.at(j).preceding.find_all{|el| !prec.include?el}]
+		else
+			self.at(i).following
 		end
-		nodes
-=end
-		prec=self.at(i).preceding
-		Hpricot::Elements[*self.at(j).preceding.find_all{|el| !prec.include?el}]
+	end
+end
+
+
+class Hpricot::Elements
+	def between(i,j)
+		Hpricot::Elements[*self.collect{|a| a.between(i,j)}]
+	end
+
+	def -(a)
+		Hpricot::Elements[*self.find_all{|el| !a.include?el}]
 	end
 end
